73389eb72515228255efdc5b4aacbc9be444f2a8
Thomas Holmes <thomasholmes@outlook.com>
2018-01-05 14:51:53 +0000

    Added random extension class and tests.

---
+++ Common.Tests/Tests/RandomExtensionTest.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RichTea.Common.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RichTea.Common.Tests.Tests
+{
+    [TestClass]
+    public class RandomExtensionTest
+    {
+        [TestMethod]
+        public void NextDoubleTest()
+        {
+            Random random = new Random();
+            double min = -10.5;
+            double max = 12.7;
+
+            int randomNumbersToGenerate = 10000;
+
+            List<double> randomResult = new List<double>();
+
+            foreach (var i in Enumerable.Range(0, randomNumbersToGenerate))
+            {
+                double randomDouble = random.NextDouble(min, max);
+                randomResult.Add(randomDouble);
+            }
+
+            int numbersBelowMin = randomResult.Count(n => n < min);
+            int numbersAboveMax = randomResult.Count(n => n >= max);
+
+            Assert.AreEqual(0, numbersBelowMin);
+            Assert.AreEqual(0, numbersAboveMax);
+        }
+
+        [TestMethod]
+        public void NextDoubleInRangeTest()
+        {
+            Random random = new Random();
+            double range = 16.7;
+            double min = -range;
+            double max = range;
+
+            int randomNumbersToGenerate = 10000;
+
+            List<double> randomResult = new List<double>();
+
+            foreach (var i in Enumerable.Range(0, randomNumbersToGenerate))
+            {
+                double randomDouble = random.NextDoubleInRange(range);
+                randomResult.Add(randomDouble);
+            }
+
+            int numbersBelowMin = randomResult.Count(n => n < min);
+            int numbersAboveMax = randomResult.Count(n => n >= max);
+
+            Assert.AreEqual(0, numbersBelowMin);
+            Assert.AreEqual(0, numbersAboveMax);
+        }
+
+        [TestMethod]
+        public void NextDoubleInRangeWithStartTest()
+        {
+            Random random = new Random();
+            double start = 9.8;
+            double range = 16.7;
+            double min = start - range;
+            double max = start + range;
+
+            int randomNumbersToGenerate = 10000;
+
+            List<double> randomResult = new List<double>();
+
+            foreach (var i in Enumerable.Range(0, randomNumbersToGenerate))
+            {
+                double randomDouble = random.NextDoubleInRange(start, range);
+                randomResult.Add(randomDouble);
+            }
+
+            int numbersBelowMin = randomResult.Count(n => n < min);
+            int numbersAboveMax = randomResult.Count(n => n >= max);
+
+            Assert.AreEqual(0, numbersBelowMin);
+            Assert.AreEqual(0, numbersAboveMax);
+        }
+    }
+}
---
+++ Common/Extensions/RandomExtension.cs
@@ -0,0 +1,48 @@
+﻿
+using System;
+
+namespace RichTea.Common.Extensions
+{
+    public static class RandomExtension
+    {
+        /// <summary>
+        /// Gets a random double between the two specified numbers.
+        /// </summary>
+        /// <param name="random">Random.</param>
+        /// <param name="min">Minimum random number.</param>
+        /// <param name="max">Maximum randon number.</param>
+        /// <returns></returns>
+        public static double NextDouble(this Random random, double min, double max)
+        {
+            double delta = max - min;
+            var next = delta * random.NextDouble();
+            var result = next + min;
+            return result;
+        }
+
+        /// <summary>
+        /// Gets a random double from 0 within +/- the supplied range.
+        /// </summary>
+        /// <param name="random"></param>
+        /// <param name="range"></param>
+        /// <returns></returns>
+        public static double NextDoubleInRange(this Random random, double range)
+        {
+            var result = random.NextDouble(-range, range);
+            return result;
+        }
+
+        /// <summary>
+        /// Gets a random double from the start value within +/- the supplied range.
+        /// </summary>
+        /// <param name="random"></param>
+        /// <param name="start"></param>
+        /// <param name="range"></param>
+        /// <returns></returns>
+        public static double NextDoubleInRange(this Random random, double start, double range)
+        {
+            var result = NextDouble(random, start - range, start + range);
+            return result;
+        }
+    }
+}
